66ace144e0b7f1c9a379e78aab3e416341a18779
Delana <dsingh@perse.co.uk>
2025-04-28 14:08:50 +0100

    Update base_1.py to add timings

--- base_1.py
+++ base_1.py
@@ -1,11 +1,30 @@
-
 radio.set_group(137)
-
+st_a, st_b = 0 , 0 
 def button_a():
-    radio.send_number(0)
+    global st_a
+    if st_a == 0:
+        radio.send_number(0)
+    else:
+        et_a = input.running_time()
+        a_time = et_a - st_a
+        basic.show_number(int(a_time))
 
 def button_b():
-    radio.send_number(1) 
+    global st_b
+    if st_b == 0:
+        radio.send_number(1)
+    else:
+        et_b = input.running_time()
+        b_time = et_b - st_b
+        basic.show_number(int(b_time))
 
+def on_received_value(name, value):
+    if name == "a2":
+        st_a = input.running_time() - value
+        
+    elif name == "b2":
+        st_b = input.running_time() - value
+
+radio.on_received_value(on_received_value)
 input.on_button_pressed(Button.A, button_a)
 input.on_button_pressed(Button.B, button_b)
