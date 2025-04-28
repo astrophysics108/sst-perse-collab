013c1df84e3db7d10de2f1d024a8b30915f49ca7
Delana <dsingh@perse.co.uk>
2025-04-28 14:41:11 +0100

    debug

--- base_1.py
+++ base_1.py
@@ -1,4 +1,7 @@
 
+
+
+
 radio.set_group(137)
 st_a = 0
 st_b = 0
@@ -8,6 +11,7 @@ def button_a():
     global st_a, done
     if st_a == 0:
         radio.send_number(0)
+        basic.show_string("A")
     else:
         et_a = input.running_time()
         a_time = et_a - st_a
@@ -24,17 +28,18 @@ def button_b():
     global st_b, done
     if st_b == 0:
         radio.send_number(1)
+        basic.show_string("B")
     else:
         et_b = input.running_time()
         b_time = et_b - st_b
         basic.show_number(int(b_time))
-    if done == 0:
-            basic.show_string("Player B wins!")
-            basic.show_number(int(b_time))
-            done = 1
-    else:
-            basic.show_string("Player A won.")
-            basic.show_number(int(b_time))
+        if done == 0:
+                basic.show_string("Player B wins!")
+                basic.show_number(int(b_time))
+                done = 1
+        else:
+                basic.show_string("Player A won.")
+                basic.show_number(int(b_time))
 
 def on_received_value(name, value):
     if name == "a2":
@@ -49,5 +54,7 @@ def main():
     input.on_button_pressed(Button.B, button_b)
 
 basic.forever(main)
+
+
 
 
