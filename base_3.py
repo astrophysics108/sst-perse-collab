cffd51cdbe321ac8a8e0eb80fa0e262dbe0befdf
Delana <dsingh@perse.co.uk>
2025-04-28 15:01:52 +0100

    debug

--- base_3.py
+++ base_3.py
@@ -1,6 +1,4 @@
 
-
-
 a_time = 0
 b_time = 0
 st_a = 0
@@ -14,7 +12,7 @@ def button_a():
         et_a = input.running_time()
         a_time = et_a - st_a
         radio.send_value("a2", a_time)
-        basic.show_icon(IconNames.HEART)
+        basic.show_icon(IconNames.YES)
         
 
 def button_b():
@@ -23,9 +21,10 @@ def button_b():
         et_b = input.running_time()
         b_time = et_b - st_b
         radio.send_value("b2", b_time)
-        basic.show_icon(IconNames.HEART)
+        basic.show_icon(IconNames.YES)
 
 def on_received_value(name, value):
+    global st_a, st_b
     if name == "a1":
         st_a = input.running_time() - value
     elif name == "b1":
@@ -33,7 +32,9 @@ def on_received_value(name, value):
         
 
 def on_forever():
+    global st_a, st_b
     radio.on_received_value(on_received_value)
     input.on_button_pressed(Button.A, button_a)
     input.on_button_pressed(Button.B, button_b)
+
 basic.forever(on_forever)
